8dae66f8c0e8d4b3dbce359614064b0a6e6841a3
BarbarianN <88117334+BarbarianN@users.noreply.github.com>
2022-05-17 16:00:47 -0300

    blue test

--- part1/src/App.js
+++ part1/src/App.js
@@ -10,7 +10,7 @@ const App = () => {
     <div className="App">
       <Mensaje color='red' message='Estamos trabajando' />
       <Mensaje color='green' message='En un curso' />
-      <Mensaje color='yellow' message='de React'/>
+      <Mensaje color='blue' message='de React'/>
       <Description />
     </div>
   )
