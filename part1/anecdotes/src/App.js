b8b4e0c88f7b4d9b29fd28833aedac21073e92fa
BarbarianN <diegooto10@gmail.com>
2022-05-19 14:51:04 -0300

    Update App.js
    final review

--- part1/anecdotes/src/App.js
+++ part1/anecdotes/src/App.js
@@ -1,50 +1,47 @@
 import { useState } from 'react'
 
+const anecdotes = [
+  'If it hurts, do it more often',
+  'Adding manpower to a late software project makes it later!',
+  'The first 90 percent of the code accounts for the first 90 percent of the development time...The remaining 10 percent of the code accounts for the other 90 percent of the development time.',
+  'Any fool can write code that a computer can understand. Good programmers write code that humans can understand.',
+  'Premature optimization is the root of all evil.',
+  'Debugging is twice as hard as writing the code in the first place. Therefore, if you write the code as cleverly as possible, you are, by definition, not smart enough to debug it.'
+]
+
+
+const Anecdote = (props) => <div><h1>{props.title}</h1>{anecdotes[props.index]}<br />has {props.points[props.index]} votes</div>  
+const Button = ({ onClick, text }) => <button onClick={onClick}>{text}</button>
+
 const App = () => {
-  const anecdotes = [
-    {text: 'If it hurts, do it more often',
-     votes: 3},
-     {text: 'Adding manpower to a late software project makes it later!',
-     votes: 2},
-     {text: 'The first 90 percent of the code accounts for the first 10 percent of the development time...The remaining 10 percent of the code accounts for the other 90 percent of the development time.',
-     votes: 0},
-     {text: 'Any fool can write code that a computer can understand. Good programmers write code that humans can understand.',
-     votes: 1},
-     {text: 'Premature optimization is the root of all evil.',
-     votes: 0},
-     {text: 'Debugging is twice as hard as writing the code in the first place. Therefore, if you write the code as cleverly as possible, you are, by definition, not smart enough to debug it.',
-     votes: 0},
-     {text: 'Programming without an extremely heavy use of console.log is same as if a doctor would refuse to use x-rays or blood tests when diagnosing patients',
-     votes: 0}
-  ]
-   
-  const [selected, setSelected] = useState({
-    index: 0,
-  })
+  const [points, setPoints] = useState(new Array(anecdotes.length +1).join('0').split('').map(parseFloat))
+  const [selected, setSelected] = useState(Math.floor(Math.random() *  anecdotes.length))
+  
+  const mostVotes = points.indexOf(Math.max(...points))
 
-  const getRandomAnecdote = () => {
-    console.log(selected.index)
-    setSelected({
-      ...selected,
-      index: Math.floor(Math.random() *  anecdotes.length), 
-    })
+  const handleVote = () => { 
+    const copy = [...points]
+    copy[selected] += 1
+    setPoints(copy)
   }
-
-  const getVote = () => {
-    //console.log("votos L "{anecdotes[selected.index].votes}")
-    console.log(anecdotes[selected.index].votes)
-    anecdotes[selected.index].votes = anecdotes[selected.index].votes +1
+  
+  const handleNextAnecdote = () => {
+    let n = selected
+    while (n === selected) { 
+      n = Math.floor(Math.random() * anecdotes.length)
+    }
+    setSelected(n)
   }
-
+  
   return (
     <div>
-      <p>{anecdotes[selected.index].text}</p>
-      <p>has {anecdotes[selected.index].votes} votes</p>
-      <br/>
-      <button onClick={getVote}>vote</button>
-      <button onClick={getRandomAnecdote}>next anecdote</button>
+      <Anecdote title='Anecdote of the day' index={selected} points={points} />
+      <Button onClick={handleVote} text='vote' />
+      <Button onClick={handleNextAnecdote} text='next anecdote' />
+      <Anecdote title='Anecdote with most votes' index={mostVotes} points={points} />
     </div>
   )
+
 }
 
 export default App
