c82a1bef510d841440425ca1ea79fa4a76104760
BarbarianN <88117334+BarbarianN@users.noreply.github.com>
2022-05-25 20:48:35 -0300

    update

--- part1/courseinfo/src/Content.js
+++ part1/courseinfo/src/Content.js
@@ -1,14 +1,14 @@
 import React from 'react'
-import Parts from './Parts.js'
 
 const Content = (promp) => {
-    const {partaux} = promp
-    console.log(partaux)
+    const {parts} = promp
+    console.log("entre a content")
+    console.log(parts)
     return(
     <div>
-      <Parts part={partaux[0].name} num1={partaux[0].exercises} />
-      <Parts part={partaux[1].name} num1={partaux[1].exercises} />
-      <Parts part={partaux[2].name} num1={partaux[2].exercises} />
+      <p>
+            {parts.name} {parts.exercises}
+          </p>
     </div>
     )
   }
--- part1/courseinfo/src/Course.js
+++ part1/courseinfo/src/Course.js
@@ -5,14 +5,15 @@ import Total from './Total.js'
 
 const Course = (promp) => {
     const {course} = promp
-    console.log({course})
+    // console.log(course.parts.exercises.reduce((a, b) => a + b, 0));
     return(
       <div>
         <Header dtl={course.name}/>
-        {course.map(courseaux => 
-            <Content key={courseaux.id} course={courseaux} />
+        {course.parts.map(parts => 
+        <Content key={parts.id} parts={parts} />
+            
         )}
-        <Content partaux={course.parts} />
+        
         <Total e1={course.parts[0].exercises} e2={course.parts[1].exercises} e3={course.parts[2].exercises} />
       </div>
     )
